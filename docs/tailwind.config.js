5ea76e38446b24590c1f873b78355ea4b2037311
San Fo <san@DellKali.HomeLab>
2023-05-26 15:45:28 +0100

    Commit 1.0

---
+++ docs/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    plugins: [
+        require('flowbite/plugin')
+    ],
+
+        content: [
+            "./**/*.{html,js}",
+            "./node_modules/flowbite/**/*.js"
+    ],
+
+    darkMode: 'class',
+
+}
