3c5deb82ac962e5be4c64456343129e251611c0a
SandforAE <Sanfo599@gmail.com>
2024-11-14 22:11:08 +0000

    restructure and Tailwind

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content');
 
 
-function PageTransitions(){
-    //Button click active class
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
-        })
+
+
+
+
+
+
+const words = ["Django Boilerplate!", "Greetings... Have a look around!", "SIgn-Up/login to unlock all features","Quick-start Django Development App."];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+currentWord = words[i];
+if (isDeleting) {
+    document.getElementById("typewriter2").textContent = currentWord.substring(0, j-1);
+    j--;
+    if (j == 0) {
+    isDeleting = false;
+    i++;
+    if (i == words.length) {
+        i = 0;
     }
-
-    //Sections Active 
-    allSections.addEventListener('click', (e) =>{
-        const id = e.target.dataset.id;
-        if(id){
-            //remove selected from the other btns
-            sectBtns.forEach((btn) =>{
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
-
-            //hide other sections
-            sections.forEach((section)=>{
-                section.classList.remove('active')
-            })
-
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
-    })
-
-    //Toggle theme
-    const themeBtn = document.querySelector('.theme-btn');
-    themeBtn.addEventListener('click',() =>{
-        let element = document.body;
-        element.classList.toggle('light-mode')
-    })
+    }
+} else {
+    document.getElementById("typewriter2").textContent = currentWord.substring(0, j+1);
+    j++;
+    if (j == currentWord.length) {
+    isDeleting = true;
+    }
+}
+setTimeout(type, 100);
 }
 
-PageTransitions();
+type();
+
+
+
+
+
+
+
+
 
 
 
